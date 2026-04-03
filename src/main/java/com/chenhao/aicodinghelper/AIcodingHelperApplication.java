0bbb15139836139cdfa071da3f32215490ee6665
chenhao <2532359224@qq.com>
2026-04-03 20:27:41 +0800

    修改前端样式

--- src/main/java/com/chenhao/aicodinghelper/AIcodingHelperApplication.java
+++ src/main/java/com/chenhao/aicodinghelper/AIcodingHelperApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.chenhao.aicodinghelper.mapper")
+@MapperScan("com.chenhao.aicodinghelper.persist.mapper")
 public class AIcodingHelperApplication {
 
     public static void main(String[] args) {
